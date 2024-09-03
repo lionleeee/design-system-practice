dd43aa161bfa285a04e4e2e006709ebcfba28cb0
lionleee <lee97hun3@gmail.com>
2024-09-03 23:44:25 +0900

    init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      xs: [
+        "12px",
+        {
+          lineHeight: "18px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      sm: [
+        "14px",
+        {
+          lineHeight: "21px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      xl: ["20px", "30px"],
+      "2xl": [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+    },
+    colors: {
+      primary: "#1d2745",
+      secondary: "#1de5d4",
+      tertiary: "#f52c50",
+      while: "#ffffff",
+      mono100: "#f1f1f1",
+      mono200: "#bebebe",
+      mono300: "#d6d7d9",
+      error: "#d01e1e",
+      social: "#395997",
+    },
+  },
+  plugins: [],
+};
